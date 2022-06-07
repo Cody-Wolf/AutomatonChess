70b25ab6942d53aba389f49742833d1d86d53594
Cody <2720299091@qq.com>
2022-06-07 16:57:58 +0800

    初步完成 Soldier 类

--- engine/game/game.cpp
+++ engine/game/game.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <glm/glm.hpp>
+#define SPEED 10.0f
+#define FIGHT_RANGE 100
 using namespace std;
 
 class Soldier {
@@ -7,16 +10,46 @@ class Soldier {
 	int team, cost, drugNum;
 	Method* method;
 	vector<BUFF*> buffs;
+	Soldier* target;
+	
 public:
+	glm::vec2 dir, pos;
+
 	Soldier(int _team) :
 		maxHP(100), HP(100), maxSP(100), SP(100),
 		method(new Normal), exp(0), level(1),
 		drugNum(2), team(_team), roundTime(1), cost(1) {};
-	void makeDecision() {}
-	void attack() {}
-	void rest() {}
-	void takingDurg() {}
-	void move() {}
+
+	void selectTarget(vector<Soldier*>& soldiers) {
+		Soldier* t;
+		double ans = 1e16;
+		for (auto tmp : soldiers)
+			if (ans < glm::distance(tmp->pos, pos))
+				ans = glm::distance(tmp->pos, pos), t = tmp;
+		if (ans < method->range) target = t;
+		dir = glm::normalize(t->pos - pos);
+	}
+
+	void makeDecision(vector<Soldier*> &soldiers) {
+		selectTarget(soldiers);
+		if (drugNum && rand() % 100 <= 80 - 100.0 * HP / maxHP){
+			takingDurg();
+		}
+		else if(rand() % 100 <= 50 - 100.0 * HP / maxHP) {
+			rest();
+		}
+		else if (target) {
+			attack(target);
+		}
+		else move();
+	}
+	void attack(Soldier* target) {}
+	void rest() { 
+		HP = max(maxHP, HP + maxHP * 0.5); 
+		SP = max(maxSP, SP + maxSP * 0.5);
+	}
+	void takingDurg() {HP = max(maxHP, HP + maxHP * 0.8), drugNum--;}
+	void move() { pos = dir * SPEED; }
 };
 
 class Wizard :Soldier {
@@ -32,11 +65,12 @@ class WarSystem {
 };
 
 class Method {
+public:
+	double range, damage;
 	virtual void attack(Soldier& target) = 0;
 };
 
 class Normal : public Method {
-	double range, damage;
 	void attack(Soldier& target) {}
 };
 
