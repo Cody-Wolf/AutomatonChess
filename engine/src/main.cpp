a3c62cd6e6e18293eac947f56be1d48ee351546a
cainiaonagi <1834493659@qq.com>
2022-06-11 20:10:49 +0800

    MSAA

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -16,6 +16,7 @@ int main()
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//GLFWwindow* window = glfwCreateWindow(1920, 1080, "AutomatonChess", glfwGetPrimaryMonitor(), nullptr);
@@ -37,6 +38,7 @@ int main()
 		return -1;
 	}
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_MULTISAMPLE);
 	Clock ck;
 	ck.setSpeed(1.0f);
 	GlobalRenderManger.init();
